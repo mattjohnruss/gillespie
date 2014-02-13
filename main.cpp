427b69b2347d8eb9fb4986280ee7b759723c53a6
Matthew Russell <matthewjohnrussell@gmail.com>
2014-02-13 19:16:23 +0000

    add missing contribution to T_0

--- main.cpp
+++ main.cpp
@@ -200,6 +200,7 @@ int main(int argc, char **argv)
         //{
             T[n_hop_left + n_hop_right + n_removal + n_inflow] =
                 T_outflow*n[n_urns-1];
+            T0 += T_outflow*n[n_urns-1];
         //}
         //else
         //{
