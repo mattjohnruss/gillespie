befc68af225cf8a6a70996020a0e57a58ea26f03
Matthew Russell <matthewjohnrussell@gmail.com>
2014-02-26 03:28:09 +0000

    awk script generator for averaging many runs

---
+++ write_average_script.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "cat output*.dat | awk '"
+echo "BEGIN {"
+echo "for (i = 0; i <= $1; i++)"
+echo "for (j = 1; j <= $2; j++)"
+echo "total[i][j] = 0"
+
+echo "} {"
+
+echo "for (j = 1; j <= $2; j++)"
+echo "total["'$1'"][j] += "'$(j+1) }'
+
+echo "END {"
+echo "for (i = 0; i <= $1; i++)"
+echo -n "print i"
+for j in $(seq 1 $2)
+do
+    echo -n ", total[i][$j]/5001"
+done
+echo ""
+echo "}"
+echo "'"
