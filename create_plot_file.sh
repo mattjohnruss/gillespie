23e97b979e7370ac798234ced20ae876dcfb868a
Matthew Russell <matthewjohnrussell@gmail.com>
2014-02-26 03:25:30 +0000

    allow arbitrary data file names in plot script

--- create_plot_file.sh
+++ create_plot_file.sh
@@ -6,11 +6,11 @@ else
 fi
 
 echo "unset key" > all.plot
-echo -n "plot \"output.dat\" $every u 1:2 w l" >> all.plot
+echo -n "plot \"$3\" $every u 1:2 w l" >> all.plot
 
 n=$(echo "$1 + 1" | bc)
 
 for i in $(seq 3 $n)
 do
-    echo -n ", \"output.dat\" $every u 1:$i w l" >> all.plot
+    echo -n ", \"$3\" $every u 1:$i w l" >> all.plot
 done
