da7eda93b3c0c1b19bb9b03eee1a8152b3904eac
Matthew Russell <matthewjohnrussell@gmail.com>
2015-10-23 15:17:55 +0100

    update Pe,Da,etc -> p,q,etc script

--- run_pe_da.bash
+++ run_pe_da.bash
@@ -1,36 +1,40 @@
-if [ "$#" -ne 10 ];
+if [ "$#" -ne 11 ];
 then
-    echo "Usage: $0 N L Pe/ep Da/ep^2 n_init t_max prefix interval last_only num_runs"
+    echo "Usage: $0 N M alpha Pe/ep Da/ep^3 n_init t_max prefix interval last_only num_runs"
     exit
 fi
 
 N=$1
-L=$2
-Pe_ep=$3
-Da_ep2=$4
-n_init=$5
-t_max=$6
-prefix=$7
-interval=$8
-last_only=$9
-num_runs=${10}
+M=$2
+alpha=$3
+Pe_ep=$4
+Da_ep3=$5
+n_init=$6
+t_max=$7
+prefix=$8
+interval=$9
+last_only=${10}
+num_runs=${11}
 
 ep=$(echo "scale = 8; 1/($N + 1)" | bc)
 Pe=$(echo "scale = 8; $Pe_ep*$ep" | bc)
-Da=$(echo "scale = 8; $Da_ep2*$ep*$ep" | bc)
-dx=$(echo "scale = 8; ($N + 1)/$L" | bc)
+Da=$(echo "scale = 8; $Da_ep3*$ep*$ep*$ep" | bc)
+dx=$(echo "scale = 8; ($N + 1)/$M" | bc)
 p=$(echo "scale = 8; (1/$dx + $Pe/2)/($Da*$dx)" | bc)
 q=$(echo "scale = 8; (1/$dx - $Pe/2)/($Da*$dx)" | bc)
 
-echo "N  = $N"
-echo "L  = $L"
-echo "dx = $dx"
-echo "p  = $p"
-echo "q  = $q"
-echo "ep = $ep"
+#n_init=$(echo "scale = 8; $alpha" | bc)
+
+echo "N     = $N"
+echo "M     = $M"
+echo "alpha = $alpha"
+echo "dx    = $dx"
+echo "p     = $p"
+echo "q     = $q"
+echo "ep    = $ep"
 echo "num_runs = $num_runs"
 
-args="-n $L -e $n_init -p $p -q $q -t $t_max -f $prefix{1}.dat"
+args="-n $M --n_init $n_init -a $alpha -b $p -p $p -q $q -t $t_max -f $prefix{1}.dat"
 
 if [ "$last_only" -eq 1 ];
 then
